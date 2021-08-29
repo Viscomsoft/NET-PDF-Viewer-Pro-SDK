ddc02735e5528ee1b611b4c06c3acf85841ac286
Viscomsoft <support@viscomsoft.com>
2021-08-29 21:26:10 +0800

    Add files via upload

---
+++ C#2019/Viewer/ExportPage.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace PDFViewerDemo
+{
+    partial class ExportPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPage = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Page No.";
+            // 
+            // txtPage
+            // 
+            this.txtPage.Location = new System.Drawing.Point(69, 35);
+            this.txtPage.Name = "txtPage";
+            this.txtPage.Size = new System.Drawing.Size(67, 20);
+            this.txtPage.TabIndex = 1;
+            this.txtPage.Text = "1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(94, 81);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 21);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ExportPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(199, 114);
+            this.ControlBox = false;
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtPage);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ExportPage";
+            this.Text = "ExportPage";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPage;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ C#2019/Viewer/FindForm.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Viscomsoft.PDFViewer;
+
+namespace PDFViewerDemo
+{
+    public partial class FindForm : Form
+    {
+        public int StartPageNumber { get; set; }
+
+        private TextSearch _searcher;
+        private bool _newSearch = true;
+
+        public FindForm(TextSearch textSearch)
+        {
+            InitializeComponent();
+
+            _searcher = textSearch;
+            _searcher.SearchProgressChanged += new EventHandler<SearchTextEventArgs>(_searcher_SearchProgressChanged);
+            _searcher.TextFound += new EventHandler(_searcher_TextFound);
+            uiFindText.Text = _searcher.Text;
+
+            this.StartPageNumber = 1;
+        }
+
+        void _searcher_TextFound(object sender, EventArgs e)
+        {
+            this.StartPageNumber = _searcher.PageNumber;
+        }
+
+        void _searcher_SearchProgressChanged(object sender, SearchTextEventArgs e)
+        {
+            if (e.NotFound)
+                showNotFound();
+            else
+                uiSearching.Text = "Searching: " + e.PageNumber;
+        }
+
+        
+        private void uiFindPrev_Click(object sender, EventArgs e)
+        {
+            if (_newSearch)
+                _searcher.findText(uiFindText.Text, this.StartPageNumber, SearchDirection.SearchBackward);
+            else
+                _searcher.findPrevious();
+
+            _newSearch = false;
+        }
+
+        private void uiFindNext_Click(object sender, EventArgs e)
+        {
+            bool result;
+            if (_newSearch)
+                _searcher.findText(uiFindText.Text, this.StartPageNumber, SearchDirection.SearchForward);
+            else
+                _searcher.findNext();
+
+            _newSearch = false;
+        }
+
+        private void uiCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void showNotFound()
+        {
+            MessageBox.Show("No more occurrence found.", "Find Text");
+        }
+
+        private void uiFindText_TextChanged(object sender, EventArgs e)
+        {
+            _newSearch = true;
+        }
+
+        private void FindForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            _searcher.SearchProgressChanged -= new EventHandler<SearchTextEventArgs>(_searcher_SearchProgressChanged);
+            _searcher.TextFound -= new EventHandler(_searcher_TextFound);
+            _searcher.cancelSearch();
+        }
+    }
+}
